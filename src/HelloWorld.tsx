81a030667b9f577543ba84ed8ee2d8c889a34b57
Pranav <38887390+thegamerhat@users.noreply.github.com>
2023-05-02 21:09:40 +0530

    Simplify

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,21 +1,12 @@
 import {useState} from 'react';
-import {continueRender, delayRender, spring} from 'remotion';
-import {
-	AbsoluteFill,
-	interpolate,
-	Sequence,
-	useCurrentFrame,
-	useVideoConfig,
-} from 'remotion';
+import {continueRender, delayRender} from 'remotion';
+import {AbsoluteFill, Sequence} from 'remotion';
 import {Logo} from './HelloWorld/Logo';
 import {Subtitle} from './HelloWorld/Subtitle';
 import {Title} from './HelloWorld/Title';
 import {trpc} from './utils/trpc';
 
-export const HelloWorld: React.FC<{
-	titleText: string;
-	titleColor: string;
-}> = ({titleText, titleColor}) => {
+export const HelloWorld: React.FC = () => {
 	const [handle] = useState(() => delayRender());
 
 	const {data} = trpc.sayHello.useQuery(
@@ -23,55 +14,16 @@ export const HelloWorld: React.FC<{
 		{onSettled: () => continueRender(handle)}
 	);
 
-	const frame = useCurrentFrame();
-	const {durationInFrames, fps} = useVideoConfig();
-
-	// Animate from 0 to 1 after 25 frames
-	const logoTranslationProgress = spring({
-		frame: frame - 25,
-		fps,
-		config: {
-			damping: 100,
-		},
-	});
-
-	// Move the logo up by 150 pixels once the transition starts
-	const logoTranslation = interpolate(
-		logoTranslationProgress,
-		[0, 1],
-		[0, -150]
-	);
-
-	// Fade out the animation at the end
-	const opacity = interpolate(
-		frame,
-		[durationInFrames - 25, durationInFrames - 15],
-		[1, 0],
-		{
-			extrapolateLeft: 'clamp',
-			extrapolateRight: 'clamp',
-		}
-	);
-
-	// A <AbsoluteFill> is just a absolutely positioned <div>!
 	return (
-		<AbsoluteFill style={{backgroundColor: 'white'}}>
-			<AbsoluteFill style={{opacity}}>
-				<AbsoluteFill style={{transform: `translateY(${logoTranslation}px)`}}>
-					<Logo />
-				</AbsoluteFill>
-				{/* Sequences can shift the time for its children! */}
-				<Sequence from={35}>
-					<Title
-						titleText={data?.greeting ?? titleText}
-						titleColor={titleColor}
-					/>
-				</Sequence>
-				{/* The subtitle will only enter on the 75th frame. */}
-				<Sequence from={75}>
-					<Subtitle />
-				</Sequence>
-			</AbsoluteFill>
+		<AbsoluteFill>
+			<Logo />
+			<Sequence from={35}>
+				<Title
+					titleText={data?.greeting ?? 'Loading...'}
+					titleColor={'black'}
+				/>
+			</Sequence>
+			<Subtitle />
 		</AbsoluteFill>
 	);
 };
